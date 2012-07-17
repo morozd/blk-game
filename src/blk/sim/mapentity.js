5f23638deeb2f5c3053e77a6e1ea4d282b17484d
Ben Vanik <ben.vanik@gmail.com>
2012-07-17 15:45:21 -0700

    Test variable showing variable replication

--- src/blk/sim/mapentity.js
+++ src/blk/sim/mapentity.js
@@ -24,10 +24,13 @@ goog.provide('blk.sim.ServerMapEntity');
 
 goog.require('blk.sim');
 goog.require('blk.sim.entities.EntityType');
+goog.require('gf.log');
 goog.require('gf.sim');
 goog.require('gf.sim.ClientEntity');
 goog.require('gf.sim.EntityState');
+goog.require('gf.sim.SchedulingPriority');
 goog.require('gf.sim.ServerEntity');
+goog.require('gf.sim.Variable');
 goog.require('goog.asserts');
 
 
@@ -80,8 +83,49 @@ blk.sim.MapEntity.State = function(entity) {
   // - sun light color
   // - fog color
   // - fog params?
+
+  /**
+   * @private
+   * @type {number}
+   */
+  this.testVar_ = 0;
+
+  /**
+   * @private
+   * @type {number}
+   */
+  this.testVarOrdinal_ = vtable.getOrdinal(blk.sim.MapEntity.State.testVarTag_);
 };
 goog.inherits(blk.sim.MapEntity.State, gf.sim.EntityState);
+
+
+/**
+ * @private
+ * @type {number}
+ */
+blk.sim.MapEntity.State.testVarTag_ = gf.sim.Variable.getUniqueTag();
+
+
+/**
+ * Gets test var.
+ * @return {number} Current value.
+ */
+blk.sim.MapEntity.State.prototype.getTestVar = function() {
+  return this.testVar_;
+};
+
+
+/**
+ * Sets test var.
+ * @param {number} value New value.
+ */
+blk.sim.MapEntity.State.prototype.setTestVar = function(value) {
+  gf.log.write('setTestVar(' + value + ')');
+  if (this.testVar_ != value) {
+    this.testVar_ = value;
+    this.setVariableDirty(this.testVarOrdinal_);
+  }
+};
 
 
 /**
@@ -89,6 +133,11 @@ goog.inherits(blk.sim.MapEntity.State, gf.sim.EntityState);
  */
 blk.sim.MapEntity.State.declareVariables = function(variableList) {
   // TODO(benvanik): add vars
+  variableList.push(new gf.sim.Variable.Float(
+      blk.sim.MapEntity.State.testVarTag_,
+      0,
+      blk.sim.MapEntity.State.prototype.getTestVar,
+      blk.sim.MapEntity.State.prototype.setTestVar));
 };
 
 
@@ -125,6 +174,19 @@ goog.mixin(blk.sim.ClientMapEntity.prototype, blk.sim.MapEntity.prototype);
 blk.sim.ServerMapEntity = function(
     simulator, entityFactory, entityId, entityFlags) {
   goog.base(this, simulator, entityFactory, entityId, entityFlags);
+
+  this.scheduleUpdate(gf.sim.SchedulingPriority.NORMAL);
 };
 goog.inherits(blk.sim.ServerMapEntity, gf.sim.ServerEntity);
 goog.mixin(blk.sim.ServerMapEntity.prototype, blk.sim.MapEntity.prototype);
+
+
+/**
+ * @override
+ */
+blk.sim.ServerMapEntity.prototype.update = function(time, timeDelta) {
+  var state = /** @type {!blk.sim.MapEntity.State} */ (this.state);
+  state.setTestVar(this.sharedMethod(time | 0));
+
+  this.scheduleUpdate(gf.sim.SchedulingPriority.NORMAL, time + 1);
+};
