f87b1f1589242d7180b6c982904a6ba1c4479789
Ben Vanik <ben.vanik@gmail.com>
2012-12-30 01:12:26 -0800

    Fixing Windows run script

--- run-server.bat
+++ run-server.bat
@@ -6,25 +6,25 @@ REM blk-game compiled server run script
 
 SET DIR=%~dp0
 IF NOT EXIST %DIR%\node_modules\blk-server\server\ GOTO NOT_NPM_INSTALL
-DIR=%DIR%\node_modules\blk-server\
+SET DIR=%DIR%\node_modules\blk-server\
 :NOT_NPM_INSTALL
 
-# Populate these values with those given to you by the browser admin
-BROWSER_URL="http://gf-browser.appspot.com/"
-SERVER_ID=""
-SERVER_KEY=""
+REM Populate these values with those given to you by the browser admin
+SET BROWSER_URL="http://gf-browser.appspot.com/"
+SET SERVER_ID=""
+SET SERVER_KEY=""
 
-# TCP port the server will be listening on
-PORT=1338
-# Name for the server in the browser
-SERVER_NAME="Server #1"
-# Maximum number of users allowed at any given time - limit based on CPU/RAM
-USERS=8
+REM TCP port the server will be listening on
+SET PORT=1338
+REM Name for the server in the browser
+SET SERVER_NAME="Server #1"
+REM Maximum number of users allowed at any given time - limit based on CPU/RAM
+SET USERS=8
 
-# Path to store maps/temp data
-FILESYSTEM=fs\
-# Map file
-MAP_PATH=maps\map01\
+REM Path to store maps/temp data
+SET FILESYSTEM=fs\
+REM Map file
+SET MAP_PATH=maps\map01\
 
 node %DIR%\server\server.js ^
     --browserUrl=%BROWSER_URL% ^
